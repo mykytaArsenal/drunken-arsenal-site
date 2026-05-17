79fd6521e74ab126705c13a6ed96835adbc5aa53
Burlachenko Mykyta <mykyta.burlachenko@reelly.io>
2026-05-17 15:04:39 +0200

    feat(i18n): add brand constants module

---
+++ lib/i18n/brand.ts
@@ -0,0 +1,4 @@
+export const BRAND = 'Drunken Arsenal';
+export const PRODUCT_NAME = 'Shot Wave';
+export const FREE_SHIPPING_THRESHOLD = '$50';
+export const SUPPORT_EMAIL = 'support@drunkenarsenal.com';
