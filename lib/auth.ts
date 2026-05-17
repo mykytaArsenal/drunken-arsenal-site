d2926d9c7ab33457cb90324246899c0bab844690
Burlachenko Mykyta <mykyta.burlachenko@reelly.io>
2026-05-17 17:58:20 +0200

    chore(lib/auth): drop unused imports

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,5 @@
-import { sql } from './db';
-import { cookies } from 'next/headers';
-
 export interface User {
   id: string;
   email: string;
   name: string | null;
 }
-
-// This file is kept for type exports only
